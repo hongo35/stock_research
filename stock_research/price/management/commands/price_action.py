bb05e6fb1aa98dd2c6d5e1fe5c4f7e2a43a68330
hongo35 <s.style3.5@gmail.com>
2015-01-24 20:22:18 +0900

    [fix] fixes #5

--- stock_research/price/management/commands/price_action.py
+++ stock_research/price/management/commands/price_action.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
+from stock_research.price.models import Price
+import jsm
+import datetime
 
 class Command(BaseCommand):
 	# args, help変数はヘルプメッセージを出力した時に使われる
 	help = "[Description]\nHere is help..."
 
 	def handle(self, *args, **options):
-		print "Test"
+		q = jsm.Quotes()
+
+		prices = Price.objects.filter(ccode = 1301)
+		print prices
